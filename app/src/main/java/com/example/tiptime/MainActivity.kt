4df531bc5db6169e216d45927597f40a47ca9ffe
mkdzt <max.detchik@gmail.com>
2022-04-11 22:11:54 +0300

    add calculate button listener

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -14,5 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnCalculate.setOnClickListener {
+            calculateTip()
+        }
+    }
+
+    private fun calculateTip() {
+        val costTip = binding.etCostOfService.text.toString().toDoubleOrNull()
+        if(costTip == null){
+            binding.tvTipResult.text = ""
+            return
+        }
+        val tipPercentage = when(binding.rgTipOptions.checkedRadioButtonId){
+            R.id.rb_option_15_percent -> 0.15
+            R.id.rb_option_18_percent -> 0.18
+            else -> 0.20
+        }
+        val roundUp = binding.swRoundUp.isChecked
+        val tip = if(roundUp) ceil(tipPercentage * costTip) else tipPercentage * costTip
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tvTipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
